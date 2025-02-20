2a10d12911ba1638100253e1181e64d68ab6ce27
unknown <parmarparth954@gmail.com>
2025-02-20 09:15:21 +0530

    complete

--- tirth-web-app/src/Pages/App/Games/Games.jsx
+++ tirth-web-app/src/Pages/App/Games/Games.jsx
@@ -16,7 +16,7 @@ const gamesData = [
     title: "METEOR",
     heroImg: "/Meteor/hero.png",
     description:
-      "Developed a skiing game where players navigate snowy terrain, collecting red flags and avoiding obstacles. Simulated realistic physics and controls for an authentic skiing experience. Introduced power-ups and bonuses to increase gameplay variety and excitement.",
+      "I developed an innovative multiplayer game where players work together to destroy incoming meteors and protect their spaceships. To keep the gameplay engaging, I designed diverse levels with increasing difficulty, ensuring a dynamic challenge for players. Additionally, I implemented a leaderboard system to track and showcase top scores, encouraging competition and motivating players to improve their performance.",
     images: ["/Meteor/1.png", "/Meteor/2.png", "/Meteor/3.png"],
     playLink: "./games",
   },
@@ -24,7 +24,7 @@ const gamesData = [
     title: "Puzzle",
     heroImg: "/Puzzle/hero.jpg",
     description:
-      "Developed a skiing game where players navigate snowy terrain, collecting red flags and avoiding obstacles. Simulated realistic physics and controls for an authentic skiing experience. Introduced power-ups and bonuses to increase gameplay variety and excitement.",
+      "I designed an interactive puzzle game where players rearrange scrambled pieces of an image to restore it to its original form. The game board consists of an image divided into a grid of tiles (3x3, 4x4, or 6x6), with one tile always left blank. Players can slide adjacent tiles into the empty space using arrow keys or WASD controls, strategically shifting pieces to reassemble the image. The challenge lies in efficiently moving the tiles to solve the puzzle in the shortest time possible, providing an engaging and rewarding experience for players.",
     images: ["/Puzzle/1.jpg", "/Puzzle/3.jpg", "/Puzzle/4.jpg"],
     playLink: "./games",
   },
@@ -32,7 +32,7 @@ const gamesData = [
     title: "Happy Garden",
     heroImg: "/Happy_Garden/logo.webp",
     description:
-      "Developed a skiing game where players navigate snowy terrain, collecting red flags and avoiding obstacles. Simulated realistic physics and controls for an authentic skiing experience. Introduced power-ups and bonuses to increase gameplay variety and excitement.",
+      "I created an interactive game where players nurture a virtual garden by watering flowers while strategically avoiding dangerous mutant fangflowers. To enhance engagement, I implemented character selection and dynamic game elements, including timed events that introduce new challenges as the game progresses. The game features vibrant visuals and collision-based interactions, ensuring an immersive and visually appealing experience for players.",
     images: [
       "/Happy_Garden/1.png",
       "/Happy_Garden/2.png",
@@ -45,12 +45,32 @@ const gamesData = [
     title: "Hitman Bunny",
     heroImg: "/Hitman_Bunny/logo.png",
     description:
-      "Developed a skiing game where players navigate snowy terrain, collecting red flags and avoiding obstacles. Simulated realistic physics and controls for an authentic skiing experience. Introduced power-ups and bonuses to increase gameplay variety and excitement.",
+      "I designed an engaging game where players defend their carrots from invading rats using interactive gameplay mechanics. To create a visually appealing experience, I developed detailed game assets, including well-crafted characters and immersive environments. Additionally, I optimized performance across various devices, ensuring smooth and consistent gameplay for all players.",
     images: [
       "/Hitman_Bunny/1.png",
       "/Hitman_Bunny/2.png",
-      "/Hitman_Bunny/3.png"
+      "/Hitman_Bunny/3.png",
     ],
+    playLink: "./games",
+  },
+  {
+    title: "Catch Cookie",
+    heroImg: "/Catch_Cookie/hero.png",
+    description:
+      "I designed a time-based game where players collect cookies while earning extra points for apples. With intuitive controls and a user-friendly interface, the gameplay remains engaging and satisfying. To enhance immersion, I incorporated dynamic sound effects and background music, creating a captivating gaming experience.",
+    images: [
+      "/Catch_Cookie/1.png",
+      "/Catch_Cookie/2.png",
+      "/Catch_Cookie/3.png",
+    ],
+    playLink: "./games",
+  },
+  {
+    title: "Flappy Bird",
+    heroImg: "/Flappy_Bird/hero.png",
+    description:
+      "I developed a challenging game where players guide a bird through obstacles, striving for high scores. The game features realistic physics-based controls for smooth and authentic movement. Additionally, I integrated social sharing features, enabling players to share their achievements with friends and compete for the top spot.",
+    images: ["/Flappy_Bird/1.png", "/Flappy_Bird/2.png", "/Flappy_Bird/3.png"],
     playLink: "./games",
   },
 ];
@@ -71,7 +91,7 @@ const Games = () => {
       <div className="container" style={{ marginBottom: "100px" }}>
         <p className="headline">Games :</p>
         <Slider {...settings}>
-          {gamesData.map((game, index) => (
+          {gamesData.slice(0, 5).map((game, index) => (
             <div key={index} className="my-slider">
               <div className="row">
                 <div className="col-lg-6 col-md-12 col-sm-12">
